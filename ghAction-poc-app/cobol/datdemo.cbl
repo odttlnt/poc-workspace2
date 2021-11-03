4fe35e08e88da56c92101eb882fb755551db2a2e
Nelson Lopez <Nelson.Lopez1@ibm.com>
2021-11-03 18:09:04 -0400

    push for via

--- ghAction-poc-app/cobol/datdemo.cbl
+++ ghAction-poc-app/cobol/datdemo.cbl
@@ -21,26 +21,12 @@
        PROCEDURE DIVISION.
       * MAIN PGM DISPLAY
       *
-           DISPLAY 'DATDEMO here- v2.5'.
+           DISPLAY 'DATDEMO Via 3.5'.
       *
       *    PERFORM FEATURE1-NEW.
       *
       *========= include copy book
            DISPLAY 'CPYBK=DATDEPND' WS-VER.
-      * do some looping
-           PERFORM VARYING num1 FROM 0 BY 1 UNTIL num1 > num2
-                IF num1 > 1  THEN
-                    perform dump_num1
-                END-IF
-           END-PERFORM.
            STOP RUN.
       *====================
-      *==================== add new features down here
-      *====================
-       dump_num1.
-           display 'Tracing num1=' num1.
-
-      *somme commit A
-      *some other commit B
-      *some chg C for feature "testing_flows"
 
