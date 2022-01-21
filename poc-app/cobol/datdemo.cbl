4ecf648b1c2ee73bd4e1129d86cc71c49bc9c75b
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-01-21 18:05:47 -0500

    reset

--- poc-app/cobol/datdemo.cbl
+++ poc-app/cobol/datdemo.cbl
@@ -12,16 +12,16 @@
           05  num1         PIC 9(3) value zeros.
           05  num2         PIC 9(3) value is 005.
       *
-       COPY DATDEPND.
-       COPY DATSHARE.
+      * COPY DATDEPND.
+      * COPY DATSHARE.
       *
        PROCEDURE DIVISION.
            DISPLAY 'DATDEMO here- v1.2209'.
       *
       *========= display copybook literals
-           DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
+      *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
       *     DISPLAY 'CPYBK=DATEMBED -> ' EMBED-VER.
-           DISPLAY 'CPYBK=DATSHARE -> ' shared-f1.
+      *     DISPLAY 'CPYBK=DATSHARE -> ' shared-f1.
 
       * do something ...
            PERFORM VARYING num1 FROM 0 BY 1 UNTIL num1 > num2
