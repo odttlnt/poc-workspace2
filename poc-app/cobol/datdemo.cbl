0771494c1ae92d8d218200fe1c266cc85a737715
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-02-09 13:54:29 -0500

    bug fix cics demo mod

--- poc-app/cobol/datdemo.cbl
+++ poc-app/cobol/datdemo.cbl
@@ -1,37 +1,23 @@
        ID DIVISION.
        PROGRAM-ID. DATDEMO.
-      *
-      * Simple demo code (NLopez) on local zDT v2.f
-      * Use Pub GITHUB project's DEVELOP branch
-      * References COMMON Copybook  DATSHARE
+      *    THIS DEMONSTRATES Impact build with CICS/BMS
+      * Tran DAT0 in rpl NLOPEZ.IDZ.LOAD
        ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WK-AREA1.
-          05  FILLER       PIC X(80).
-          05  num1         PIC 9(3) value zeros.
-          05  num2         PIC 9(3) value is 005.
       *
-      * COPY DATDEPND.
-      * COPY DATSHARE.
+      *    COPY DFHAID.
       *
+      * My test map
+           COPY DATMAP.
        PROCEDURE DIVISION.
-           DISPLAY 'DATDEMO here- v1.2209'.
+           DISPLAY 'Sending a test map'.
+           EXEC CICS
+                SEND MAP ('DATMAP')
+                     MAPSET('DATMLIS')
+                     FROM(DATMLISO)
+           END-EXEC.
       *
-      *========= display copybook literals
-      *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
-      *     DISPLAY 'CPYBK=DATEMBED -> ' EMBED-VER.
-      *     DISPLAY 'CPYBK=DATSHARE -> ' shared-f1.
-
-      * do something ...
-           PERFORM VARYING num1 FROM 0 BY 1 UNTIL num1 > num2
-                IF num1 > 1  THEN
-                    perform dump_num1
-                END-IF
-           END-PERFORM.
-      *========== static call example
            CALL 'DATSUB'.
            STOP RUN.
-      *==========
-       dump_num1.
-           display 'Tracing num1=' num1.
