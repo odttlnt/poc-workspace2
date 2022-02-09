95076d61ed6468d88dbb8c1347fb9852d52ce4d2
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-02-09 15:56:29 -0500

    replace

---
+++ poc-app/cobol/datxcics.cbl
@@ -0,0 +1,37 @@
+       ID DIVISION.
+       PROGRAM-ID. DATDEMO.
+      *
+      * Simple demo code (NLopez) on local zDT v2.f
+      * Use Pub GITHUB project's DEVELOP branch
+      * References COMMON Copybook  DATSHARE
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WK-AREA1.
+          05  FILLER       PIC X(80).
+          05  num1         PIC 9(3) value zeros.
+          05  num2         PIC 9(3) value is 005.
+      *
+      * COPY DATDEPND.
+      * COPY DATSHARE.
+      *
+       PROCEDURE DIVISION.
+           DISPLAY 'DATDEMO here- v1.2209'.
+      *
+      *========= display copybook literals
+      *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
+      *     DISPLAY 'CPYBK=DATEMBED -> ' EMBED-VER.
+      *     DISPLAY 'CPYBK=DATSHARE -> ' shared-f1.
+
+      * do something ...
+           PERFORM VARYING num1 FROM 0 BY 1 UNTIL num1 > num2
+                IF num1 > 1  THEN
+                    perform dump_num1
+                END-IF
+           END-PERFORM.
+      *========== static call example
+           CALL 'DATSUB'.
+           STOP RUN.
+      *==========
+       dump_num1.
+           display 'Tracing num1=' num1.
