67345ba7abc1cc2bb59b499cc3b24e3910d5e1ba
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-02-09 16:05:10 -0500

    redo map and cics

--- poc-app/cobol/datdemo.cbl
+++ poc-app/cobol/datdemo.cbl
@@ -1,7 +1,7 @@
        ID DIVISION.
        PROGRAM-ID. DATDEMO.
       *    THIS DEMONSTRATES Impact build with CICS/BMS
-      * Tran DAT0 in rpl NLOPEZ.IDZ.LOAD
+      * Tran DAT0 in rpl NLOPEZ.IDZ.LOAD  v2
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        DATA DIVISION.
