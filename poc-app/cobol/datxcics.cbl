176ca3a51ccda31c3c5c9dc077013683f83dc45d
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-02-09 16:01:12 -0500

    redo

--- poc-app/cobol/datxcics.cbl
+++ poc-app/cobol/datxcics.cbl
@@ -1,37 +1,11 @@
        ID DIVISION.
-       PROGRAM-ID. DATDEMO.
+       PROGRAM-ID. DATAXCICS.
       *
-      * Simple demo code (NLopez) on local zDT v2.f
-      * Use Pub GITHUB project's DEVELOP branch
-      * References COMMON Copybook  DATSHARE
+      * a dup of datbatc. Dont ask Not really cics
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WK-AREA1.
-          05  FILLER       PIC X(80).
-          05  num1         PIC 9(3) value zeros.
-          05  num2         PIC 9(3) value is 005.
-      *
-      * COPY DATDEPND.
-      * COPY DATSHARE.
-      *
-       PROCEDURE DIVISION.
-           DISPLAY 'DATDEMO here- v1.2209'.
-      *
-      *========= display copybook literals
-      *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
-      *     DISPLAY 'CPYBK=DATEMBED -> ' EMBED-VER.
-      *     DISPLAY 'CPYBK=DATSHARE -> ' shared-f1.
 
-      * do something ...
-           PERFORM VARYING num1 FROM 0 BY 1 UNTIL num1 > num2
-                IF num1 > 1  THEN
-                    perform dump_num1
-                END-IF
-           END-PERFORM.
-      *========== static call example
-           CALL 'DATSUB'.
+       PROCEDURE DIVISION.
+           DISPLAY 'hello ball ... - v1.2209'.
            STOP RUN.
-      *==========
-       dump_num1.
-           display 'Tracing num1=' num1.
