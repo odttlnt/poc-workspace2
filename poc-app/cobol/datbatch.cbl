0bad76598f4b4fc40bbfd5ff781a421b2324c4d0
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-03-15 08:43:13 -0500

    test ucd with batch, cics and db2

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,7 +1,7 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
       *
-      * Simple demo code (NLopez) on local zDT v2.f
+      * Simple demo code (NLopez) on local zDT v2.G
       * Use Pub GITHUB project's DEVELOP branch
       * References COMMON Copybook  DATSHARE
        ENVIRONMENT DIVISION.
