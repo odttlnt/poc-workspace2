6afb05f625db6f510487f1b00be146ad31de5414
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-03-21 13:00:24 -0400

    retest

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,7 +1,7 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
       *
-      * Simple demo code (NLopez) on local zDT v6.7
+      * Simple demo code (NLopez) on local zDT v6.8
       * Use Pub GITHUB project's DEVELOP branch
       * References COMMON Copybook  DATSHARE
        ENVIRONMENT DIVISION.
