6a347876615366376c4d8273594026093eac2267
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-04-01 13:11:22 -0400

    fix header -

--- poc-app/cpp/cppdemo.cpp
+++ poc-app/cpp/cppdemo.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cpphead>
-// see https://www.ibm.com/docs/en/zos/2.1.0?topic=compiling-input-zos-xl-cc-compiler
-
 int main() {
     std::cout << "Hello World!";
     return 0;
