45f05d08bf783a6c7365af63c6a1ead48a369205
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-07-05 13:43:54 -0400

    test 10:50

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -16,7 +16,7 @@
       * COPY DATSHARE.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH here- v10:49'.
+           DISPLAY 'DATBATCH here- v10:50'.
       *
       *========= display copybook literals
       *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
