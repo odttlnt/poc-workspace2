1c4281b9e1d4591f227a298ad7f0f15d46919803
Nelson Lopez <Nelson.Lopez1@ibm.com>
2022-08-17 19:49:02 -0400

    push my feature change to git

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -16,7 +16,7 @@
       * COPY DATSHARE.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH here- Wazi nelson'.
+           DISPLAY 'DATBATCH Test Demo'.
       *
       *========= display copybook literals
       *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
