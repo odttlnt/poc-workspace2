11b43ecedb8136020514c8ba59b8c9b464315fdb
nlopez1-ibm <62708912+nlopez1-ibm@users.noreply.github.com>
2022-11-05 08:37:09 -0400

    Update datbatch.cbl

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -16,7 +16,7 @@
       * COPY DATSHARE.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH Demo UCD verb 113'.
+           DISPLAY 'DATBATCH Demo UCD verb 114'.
       *
       *========= display copybook literals
       *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
